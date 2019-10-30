1ff82fb8bf674431d25310f54db09a4c4f80349f
wressley <johnson.kyle.eric@gmail.com>
2019-10-29 20:29:18 -0400

    Add project files.

---
+++ PlantDiary002/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PlantDiary002.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            int yearStarted = 2006;
+            string name = "My Plant Diary";
+            ViewData["Name"] = name;
+
+        }
+    }
+}
